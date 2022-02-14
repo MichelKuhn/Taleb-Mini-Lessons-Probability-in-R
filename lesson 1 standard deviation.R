af56c73fe72f6cdf0aaf3fa177091ef1607b5ec5
MichelKuhn <kichel.moon@gmail.com>
2022-02-14 18:00:58 +0100

    Lesson 1 and 2

---
+++ lesson 1 standard deviation.R
@@ -0,0 +1,24 @@
+#Lesson 1: Standard Deviation vs Mean Absolute Deviation
+########################################################
+install.packages("ie2misc")
+library("ie2misc")
+
+#In Mediokristan:
+mediokristan <- rnorm(100000, mean=10, sd=2)
+
+#Both are similar (~2)
+sd(mediokristan)
+madstat(mediokristan)
+
+#SD/MAD should be around sqrt(pi/2) in Mediokristan
+sd(mediokristan) / madstat(mediokristan)
+sqrt(pi/2)
+
+#In Extremistan:
+extremistan <- c(replicate(999999, 0), 10^6)
+
+sd(extremistan)
+madstat(extremistan)
+
+#SD/MAD is huuuuge
+sd(extremistan) / madstat(extremistan)
---
+++ lesson 2 fat tails.R
@@ -0,0 +1,13 @@
+#Lesson 2: Fat Tails
+####################
+
+#The mean is one, only from one observation!
+extremistan <- c(replicate(999999, 0), 10^6)
+mean(extremistan)
+
+#Fatten the tail of a gaussian: More events in the middle!
+#Both have the same mean, but the extreme event is rarer (and deadlier) in the second one
+fat_tail <- c(rnorm(99, mean=10, sd=2), 10^3)
+fatter_tail <- c(rnorm(999, mean=10, sd=2), 10^4)
+mean(fat_tail)
+mean(fatter_tail)
